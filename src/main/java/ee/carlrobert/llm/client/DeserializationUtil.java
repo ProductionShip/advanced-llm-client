6754ec8f37791049692250595e830280fb4cd30a
ProductionShip <165349011+ProductionShip@users.noreply.github.com>
2022-09-09 14:16:53 +0000

    fixed url

--- src/main/java/ee/carlrobert/llm/client/DeserializationUtil.java
+++ src/main/java/ee/carlrobert/llm/client/DeserializationUtil.java
@@ -2,4 +2,15 @@ package ee.carlrobert.llm.client;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import java.io.IOExcept
+import java.io.IOException;
+import okhttp3.Response;
+
+public class DeserializationUtil {
+
+  private DeserializationUtil() {
+  }
+
+  public static final ObjectMapper OBJECT_MAPPER = new ObjectMapper()
+      .setSerializationInclusion(JsonInclude.Include.NON_NULL);
+
+  public stati
