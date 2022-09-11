fe7e27caacbbce9e50057366a39958a243033974
ProductionShip <165349011+ProductionShip@users.noreply.github.com>
2022-09-11 14:21:13 +0000

    reset version

--- src/main/java/ee/carlrobert/llm/client/DeserializationUtil.java
+++ src/main/java/ee/carlrobert/llm/client/DeserializationUtil.java
@@ -13,4 +13,9 @@ public class DeserializationUtil {
   public static final ObjectMapper OBJECT_MAPPER = new ObjectMapper()
       .setSerializationInclusion(JsonInclude.Include.NON_NULL);
 
-  public stati
+  public static <T> T mapResponse(Response response, Class<T> clazz) {
+    var body = response.body();
+    try {
+      return OBJECT_MAPPER.readValue(body.string(), clazz);
+    } catch (IOException ex) {
+      throw new RuntimeException("Could not deserialize response
