c32d747d6d9f2bab79d887e30b6add81d54a8a34
ProductionShip <165349011+ProductionShip@users.noreply.github.com>
2022-09-12 18:29:50 +0000

    Updated tutorial

--- src/main/java/ee/carlrobert/llm/client/InterceptorUtil.java
+++ src/main/java/ee/carlrobert/llm/client/InterceptorUtil.java
@@ -9,4 +9,7 @@ import okio.BufferedSource;
 
 public class InterceptorUtil {
 
-  pu
+  public static final Interceptor REWRITE_X_NDJSON_CONTENT_INTERCEPTOR = chain -> {
+    Response response = chain.proceed(chain.request());
+    if ("application/x-ndjson".equals(response.header("Content-Type", ""))) {
+      try (ResponseBod
