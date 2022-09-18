587e846963adc4a4f4a97c420475f6d12d9ec4f7
ProductionShip <165349011+ProductionShip@users.noreply.github.com>
2022-09-18 09:20:56 +0000

    Added Readme file

--- src/main/java/ee/carlrobert/llm/client/anthropic/ClaudeClient.java
+++ src/main/java/ee/carlrobert/llm/client/anthropic/ClaudeClient.java
@@ -5,4 +5,8 @@ import static ee.carlrobert.llm.client.DeserializationUtil.OBJECT_MAPPER;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import ee.carlrobert.llm.PropertiesLoader;
 import ee.carlrobert.llm.client.DeserializationUtil;
-import ee.carlrobert.llm.client.anthropic.completion.ClaudeCompletionErrorDet
+import ee.carlrobert.llm.client.anthropic.completion.ClaudeCompletionErrorDetails;
+import ee.carlrobert.llm.client.anthropic.completion.ClaudeCompletionRequest;
+import ee.carlrobert.llm.client.anthropic.completion.ClaudeCompletionResponse;
+import ee.carlrobert.llm.client.anthropic.completion.ClaudeCompletionStreamResponse;
+i
