4e809a5ae0a373a1cb3bbb1b7ec80904e521ab2f
ProductionShip <165349011+ProductionShip@users.noreply.github.com>
2022-10-11 17:27:50 +0000

    Fix gitignore.

--- src/main/java/ee/carlrobert/llm/client/anthropic/completion/ClaudeBase64Source.java
+++ src/main/java/ee/carlrobert/llm/client/anthropic/completion/ClaudeBase64Source.java
@@ -3,4 +3,12 @@ package ee.carlrobert.llm.client.anthropic.completion;
 import com.fasterxml.jackson.annotation.JsonGetter;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.anno
+import com.fasterxml.jackson.annotation.JsonSetter;
+import com.fasterxml.jackson.annotation.JsonTypeName;
+import java.util.Base64;
+
+/**
+ * Represents a base64-encoded data source in Claude.
+ * <br>
+ * Allowed media types include the following:
+ * image/jpeg, image/png, image/gif, and im
