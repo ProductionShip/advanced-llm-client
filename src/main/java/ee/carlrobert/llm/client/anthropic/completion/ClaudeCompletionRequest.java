26d7b9948d73f2a31f1808fa7ac253c72d9e9e74
ProductionShip <165349011+ProductionShip@users.noreply.github.com>
2022-10-19 11:45:52 +0000

    master FI

--- src/main/java/ee/carlrobert/llm/client/anthropic/completion/ClaudeCompletionRequest.java
+++ src/main/java/ee/carlrobert/llm/client/anthropic/completion/ClaudeCompletionRequest.java
@@ -1,1 +1,11 @@
-package ee.carlrobert.llm.client.anthropic.complet
+package ee.carlrobert.llm.client.anthropic.completion;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import ee.carlrobert.llm.completion.CompletionRequest;
+import java.util.List;
+
+public class ClaudeCompletionRequest implements CompletionRequest {
+
+  private String model;
+  private String system;
+  private List<ClaudeComplet
