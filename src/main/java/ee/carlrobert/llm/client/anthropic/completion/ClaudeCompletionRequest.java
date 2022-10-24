7758b4d88454371681706c61b7d052b52b21ebff
ProductionShip <165349011+ProductionShip@users.noreply.github.com>
2022-10-24 06:41:37 +0000

    Known issues

--- src/main/java/ee/carlrobert/llm/client/anthropic/completion/ClaudeCompletionRequest.java
+++ src/main/java/ee/carlrobert/llm/client/anthropic/completion/ClaudeCompletionRequest.java
@@ -8,4 +8,18 @@ public class ClaudeCompletionRequest implements CompletionRequest {
 
   private String model;
   private String system;
-  private List<ClaudeComplet
+  private List<ClaudeCompletionMessage> messages;
+  @JsonProperty("max_tokens")
+  private int maxTokens;
+  private boolean stream;
+  private double temperature;
+  @JsonProperty("top_k")
+  private int topK;
+  @JsonProperty("top_p")
+  private int topP;
+
+  public String getModel() {
+    return model;
+  }
+
+  public vo
