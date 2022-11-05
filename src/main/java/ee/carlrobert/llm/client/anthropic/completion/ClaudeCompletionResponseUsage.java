f9030e3ac484e6cd2fa74d80277af3fd57cb4ce3
ProductionShip <165349011+ProductionShip@users.noreply.github.com>
2022-11-05 09:59:33 +0000

    clean unused code

--- src/main/java/ee/carlrobert/llm/client/anthropic/completion/ClaudeCompletionResponseUsage.java
+++ src/main/java/ee/carlrobert/llm/client/anthropic/completion/ClaudeCompletionResponseUsage.java
@@ -1,1 +1,7 @@
-package ee
+package ee.carlrobert.llm.client.anthropic.completion;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class ClaudeCompletionResponseUsage {
