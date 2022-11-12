719c4bddea621f0d2bb1d647fdd6ae7c80287a6d
ProductionShip <165349011+ProductionShip@users.noreply.github.com>
2022-11-12 18:26:59 +0000

    Set release date.

--- src/main/java/ee/carlrobert/llm/client/anthropic/completion/ClaudeCompletionStreamResponse.java
+++ src/main/java/ee/carlrobert/llm/client/anthropic/completion/ClaudeCompletionStreamResponse.java
@@ -9,3 +9,21 @@ public class ClaudeCompletionStreamResponse implements CompletionResponse {
   private String type;
   private int index;
   private ClaudeCompletionResponseMessage delta;
+
+  public String getType() {
+    return type;
+  }
+
+  public void setType(String type) {
+    this.type = type;
+  }
+
+  public int getIndex() {
+    return index;
+  }
+
+  public void setIndex(int index) {
+    this.index = index;
+  }
+
+  public ClaudeCompletionRespon
