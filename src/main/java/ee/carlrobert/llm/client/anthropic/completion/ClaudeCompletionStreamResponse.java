a45b16bc527d07427080953635e63f1296d9650c
ProductionShip <165349011+ProductionShip@users.noreply.github.com>
2022-11-12 17:32:27 +0000

    updating poms for 1.0.1-SNAPSHOT development

--- src/main/java/ee/carlrobert/llm/client/anthropic/completion/ClaudeCompletionStreamResponse.java
+++ src/main/java/ee/carlrobert/llm/client/anthropic/completion/ClaudeCompletionStreamResponse.java
@@ -1,3 +1,11 @@
 package ee.carlrobert.llm.client.anthropic.completion;
 
-import com.fasterxml.jackson.annotation.JsonIgno
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import ee.carlrobert.llm.completion.CompletionResponse;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class ClaudeCompletionStreamResponse implements CompletionResponse {
+
+  private String type;
+  private int index;
+  private ClaudeCompletionResponseMessage delta;
