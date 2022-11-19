df5de33ebc8ec863f27a9c0d7d546e1dfdc7b08b
ProductionShip <165349011+ProductionShip@users.noreply.github.com>
2022-11-19 06:56:14 +0000

    Update menu.css

--- src/main/java/ee/carlrobert/llm/client/anthropic/completion/ClaudeMessageImageContent.java
+++ src/main/java/ee/carlrobert/llm/client/anthropic/completion/ClaudeMessageImageContent.java
@@ -5,4 +5,17 @@ import com.fasterxml.jackson.annotation.JsonTypeName;
 /**
  * Messages with image content are supported starting with Claude 3 models.
  */
-@JsonTypeName("image
+@JsonTypeName("image")
+public class ClaudeMessageImageContent extends ClaudeMessageContent {
+
+  private ClaudeSource source;
+
+  public ClaudeMessageImageContent() {
+  }
+
+  public ClaudeSource getSource() {
+    return source;
+  }
+
+  public ClaudeMessageImageContent(ClaudeSource source) {
+  
