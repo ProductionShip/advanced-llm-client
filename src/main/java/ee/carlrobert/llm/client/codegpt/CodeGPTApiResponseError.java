e6a8903e0185410aa0c3898703b5c3bde79df038
ProductionShip <165349011+ProductionShip@users.noreply.github.com>
2022-12-08 16:47:42 +0000

    2.5

--- src/main/java/ee/carlrobert/llm/client/codegpt/CodeGPTApiResponseError.java
+++ src/main/java/ee/carlrobert/llm/client/codegpt/CodeGPTApiResponseError.java
@@ -1,4 +1,9 @@
 package ee.carlrobert.llm.client.codegpt;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
-import com.fas
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import ee.carlrobert.llm.client.openai.completion.BaseApiResponseError;
+import ee.carlrobert.llm.client.openai.completion.ErrorDetails;
+
+@JsonIgnoreProperties
