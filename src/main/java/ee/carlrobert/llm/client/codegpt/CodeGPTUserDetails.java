7ee64a362942fa523c5a354b86eeb98a9ed5de09
ProductionShip <165349011+ProductionShip@users.noreply.github.com>
2022-12-18 10:01:40 +0000

    Bump ssri from 6.0.1 to 6.0.2

--- src/main/java/ee/carlrobert/llm/client/codegpt/CodeGPTUserDetails.java
+++ src/main/java/ee/carlrobert/llm/client/codegpt/CodeGPTUserDetails.java
@@ -1,4 +1,13 @@
 package ee.carlrobert.llm.client.codegpt;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
-import com.fasterxml.jackson.annotatio
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import java.util.List;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class CodeGPTUserDetails {
+
+  private final String fullName;
+  private final PricingPlan pricingPlan;
+  private fina
