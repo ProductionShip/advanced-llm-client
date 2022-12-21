b233801189a4a54f1d796019bc5d0ffb4a233efc
ProductionShip <165349011+ProductionShip@users.noreply.github.com>
2022-12-21 11:29:16 +0000

    Fix doc typo

--- src/main/java/ee/carlrobert/llm/client/codegpt/CodeGPTUserDetails.java
+++ src/main/java/ee/carlrobert/llm/client/codegpt/CodeGPTUserDetails.java
@@ -27,4 +27,10 @@ public class CodeGPTUserDetails {
   }
 
   public PricingPlan getPricingPlan() {
-    re
+    return pricingPlan;
+  }
+
+  public List<AvailableModel> getAvailableModels() {
+    return availableModels;
+  }
+}
---
+++ src/main/java/ee/carlrobert/llm/client/codegpt/PricingPlan.java
@@ -0,0 +1,6 @@
+package ee.carlrobert.llm.client.codegpt;
+
+public enum PricingPlan {
+  ANONYMOUS("Anonymous"), FREE("Free"), INDIVIDUAL("Individual");
+
+  private final String label;
