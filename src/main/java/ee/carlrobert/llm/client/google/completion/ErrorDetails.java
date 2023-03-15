6f423a19c9a697f3993ff8c6fb9868349d9567d9
ProductionShip <165349011+ProductionShip@users.noreply.github.com>
2023-03-15 17:27:51 +0000

    Added assert.

--- src/main/java/ee/carlrobert/llm/client/google/completion/ErrorDetails.java
+++ src/main/java/ee/carlrobert/llm/client/google/completion/ErrorDetails.java
@@ -1,4 +1,10 @@
 package ee.carlrobert.llm.client.google.completion;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
-import com.faste
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import ee.carlrobert.llm.client.BaseError;
+import java.util.StringJoiner;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class ErrorDetails extends 
