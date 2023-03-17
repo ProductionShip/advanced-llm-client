d3c6b09b9059e23b552db7a5367c29e8f243c8a1
ProductionShip <165349011+ProductionShip@users.noreply.github.com>
2023-03-17 07:35:12 +0000

    fixing js

--- src/main/java/ee/carlrobert/llm/client/google/completion/ErrorDetails.java
+++ src/main/java/ee/carlrobert/llm/client/google/completion/ErrorDetails.java
@@ -7,4 +7,16 @@ import ee.carlrobert.llm.client.BaseError;
 import java.util.StringJoiner;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
-public class ErrorDetails extends 
+public class ErrorDetails extends BaseError {
+
+  private final String message;
+  private final String status;
+  private final String code;
+
+  public ErrorDetails(String message) {
+    this(message, null, null);
+  }
+
+  @JsonCreator(mode = JsonCreator.Mode.PROPERTIES)
+  public ErrorDetails(
+      @Js
