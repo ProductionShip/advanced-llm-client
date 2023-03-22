2eea7db3d5eed2933a288b49d86abb37abbb1755
ProductionShip <165349011+ProductionShip@users.noreply.github.com>
2023-03-22 16:03:21 +0000

    kde4_header and kde4_footer() are gone now

--- src/main/java/ee/carlrobert/llm/client/google/completion/GoogleContentPart.java
+++ src/main/java/ee/carlrobert/llm/client/google/completion/GoogleContentPart.java
@@ -8,4 +8,19 @@ import java.util.Base64.Encoder;
 @JsonInclude(Include.NON_NULL)
 public class GoogleContentPart {
 
- 
+  private String text;
+  private Blob inlineData;
+
+  public GoogleContentPart() {
+  }
+
+  public GoogleContentPart(String text) {
+    this(text, null);
+  }
+
+  public GoogleContentPart(String text, Blob inlineData) {
+    this.text = text;
+    this.inlineData = inlineData;
+  }
+
+  public S
