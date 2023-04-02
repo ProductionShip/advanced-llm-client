7aa7844303aa384e305b1d31fb64d30852776ae2
ProductionShip <165349011+ProductionShip@users.noreply.github.com>
2023-04-02 10:05:56 +0000

    .idea

--- src/main/java/ee/carlrobert/llm/client/google/completion/GoogleGenerationConfig.java
+++ src/main/java/ee/carlrobert/llm/client/google/completion/GoogleGenerationConfig.java
@@ -8,3 +8,12 @@ import java.util.List;
  */
 public class GoogleGenerationConfig {
 
+  private final List<String> stopSequences;
+  private final int candidateCount;
+  private final double temperature;
+  private final int maxOutputTokens;
+  private final double topP;
+  private final int topK;
+
+  public GoogleGenerationConfig(Builder builder) {
+    this.stopSeque
