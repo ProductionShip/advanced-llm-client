d653cae96c3be9d3e9d39350f468bd7ed478c511
ProductionShip <165349011+ProductionShip@users.noreply.github.com>
2023-04-15 22:20:29 +0000

    Initial Check-In

--- src/main/java/ee/carlrobert/llm/client/google/embedding/GoogleEmbeddingRequest.java
+++ src/main/java/ee/carlrobert/llm/client/google/embedding/GoogleEmbeddingRequest.java
@@ -2,4 +2,12 @@ package ee.carlrobert.llm.client.google.embedding;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
-import ee.car
+import ee.carlrobert.llm.client.google.completion.GoogleCompletionContent;
+import java.util.List;
+
+@JsonInclude(Include.NON_NULL)
+public class GoogleEmbeddingRequest {
+
+  private final GoogleCompletionContent content;
+  private final TaskType taskType;
+  private 
