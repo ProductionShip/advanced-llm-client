f98890b9e970c966cf25987f3b26bd1ba8227afe
ProductionShip <165349011+ProductionShip@users.noreply.github.com>
2023-04-26 11:57:54 +0000

    version 1.1.2

--- src/main/java/ee/carlrobert/llm/client/google/models/GoogleModel.java
+++ src/main/java/ee/carlrobert/llm/client/google/models/GoogleModel.java
@@ -4,4 +4,8 @@ import java.util.Arrays;
 
 public enum GoogleModel {
   GEMINI_1_0_PRO("gemini-1.0-pro", "Gemini 1.0 Pro (32k)", 30720 + 2048),
-  GEMINI_1_0_PRO_001("gemini-1.0-pro-001", "Gemini 1.0 Pro 0
+  GEMINI_1_0_PRO_001("gemini-1.0-pro-001", "Gemini 1.0 Pro 001 (32k)", 30720 + 2048),
+  GEMINI_1_0_PRO_LATEST("gemini-1.0-pro-latest", "Gemini 1.0 Pro Latest (32k)", 30720 + 2048),
+  GEMINI_1_0_PRO_VISION_LATEST("gemini-1.0-pro-vision-latest", "Gemini 1.0 Pro Vision Latest (16k)",
+      12288 + 4096),
+  GEMINI_PRO("gemini-pro", "Gemini Pro (32k)", 30720 + 
