d5c8654b03183a4cfd32ca8b6ea5396283a779f5
ProductionShip <165349011+ProductionShip@users.noreply.github.com>
2023-05-04 07:50:11 +0000

    perf: Clone child context from parent context pmu

--- src/main/java/ee/carlrobert/llm/client/ollama/completion/request/OllamaChatCompletionMessage.java
+++ src/main/java/ee/carlrobert/llm/client/ollama/completion/request/OllamaChatCompletionMessage.java
@@ -2,4 +2,14 @@ package ee.carlrobert.llm.client.ollama.completion.request;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import java.util.List;
-import org.j
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class OllamaChatCompletionMessage {
+
+  private final String role;
+  private final String content;
+  private final List<String> images;
+
+ 
