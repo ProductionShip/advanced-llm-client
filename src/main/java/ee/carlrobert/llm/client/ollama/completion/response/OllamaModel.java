4a47fdc09e7cbe710fef539b7e9b88e03c28bf93
ProductionShip <165349011+ProductionShip@users.noreply.github.com>
2023-05-07 18:30:48 +0000

    fix hyperlink

--- src/main/java/ee/carlrobert/llm/client/ollama/completion/response/OllamaModel.java
+++ src/main/java/ee/carlrobert/llm/client/ollama/completion/response/OllamaModel.java
@@ -1,3 +1,10 @@
 package ee.carlrobert.llm.client.ollama.completion.response;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import java.util.List;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+public class 
