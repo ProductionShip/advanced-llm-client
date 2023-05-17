e75d51ac1e566aedb5cf8e8f3014ab140286d37c
ProductionShip <165349011+ProductionShip@users.noreply.github.com>
2023-05-17 06:44:48 +0000

    README.txt

--- src/main/java/ee/carlrobert/llm/client/ollama/completion/response/OllamaTagsResponse.java
+++ src/main/java/ee/carlrobert/llm/client/ollama/completion/response/OllamaTagsResponse.java
@@ -1,3 +1,9 @@
 package ee.carlrobert.llm.client.ollama.completion.response;
 
-import com.fast
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import java.util.List;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonNaming(Prop
