ef4541c4923ac9c6b24db2200e58fc9ae3ee95b7
ProductionShip <165349011+ProductionShip@users.noreply.github.com>
2023-05-18 13:02:30 +0000

    Debuging

--- src/main/java/ee/carlrobert/llm/client/openai/completion/ApiResponseError.java
+++ src/main/java/ee/carlrobert/llm/client/openai/completion/ApiResponseError.java
@@ -5,4 +5,16 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
-public class ApiResponseError implements BaseApiR
+public class ApiResponseError implements BaseApiResponseError {
+
+  private final ErrorDetails error;
+
+  @JsonCreator(mode = JsonCreator.Mode.PROPERTIES)
+  public ApiResponseError(@JsonProperty("error") ErrorDetails error) {
+    this.error = error;
+  }
+
+  public ErrorDetails getError() {
+    return error;
+  }
+}
