f6d740adac0fa63ed3ec03d66131ba0fbae10b8b
ProductionShip <165349011+ProductionShip@users.noreply.github.com>
2023-05-20 17:23:29 +0000

    Updated composer.json.

--- src/main/java/ee/carlrobert/llm/client/openai/completion/ErrorDetails.java
+++ src/main/java/ee/carlrobert/llm/client/openai/completion/ErrorDetails.java
@@ -1,4 +1,12 @@
 package ee.carlrobert.llm.client.openai.completion;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
-import com.fasterxml.jack
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import ee.carlrobert.llm.client.BaseError;
+import java.util.StringJoiner;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class ErrorDetails extends BaseError {
+
+  private static fin
