f07f00beccac0d21d3556ef733d7d0ff50712d0b
ProductionShip <165349011+ProductionShip@users.noreply.github.com>
2023-06-15 08:55:27 +0000

    rename package name

--- src/main/java/ee/carlrobert/llm/client/openai/completion/OpenAITextCompletionEventSourceListener.java
+++ src/main/java/ee/carlrobert/llm/client/openai/completion/OpenAITextCompletionEventSourceListener.java
@@ -4,4 +4,10 @@ import static ee.carlrobert.llm.client.DeserializationUtil.OBJECT_MAPPER;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import ee.carlrobert.llm.client.openai.completion.response.OpenAITextCompletionResponse;
-import ee.carlrobert.llm.client.openai.completion.response.OpenAITextCompletionRes
+import ee.carlrobert.llm.client.openai.completion.response.OpenAITextCompletionResponseChoice;
+import ee.carlrobert.llm.completion.CompletionEventListener;
+import ee.carlrobert.llm.completion.CompletionEventSourceListener;
+import java.util.Objects;
+import java.util.stream.Stream;
+
+public class OpenAITextCompletionEvent
