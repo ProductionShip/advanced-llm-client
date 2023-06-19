9715346770f90c23c7b53f726d5d4284cc271bf7
ProductionShip <165349011+ProductionShip@users.noreply.github.com>
2023-06-19 09:20:45 +0000

    =^_^= Update templates

--- src/main/java/ee/carlrobert/llm/client/openai/completion/request/OpenAIChatCompletionDetailedMessage.java
+++ src/main/java/ee/carlrobert/llm/client/openai/completion/request/OpenAIChatCompletionDetailedMessage.java
@@ -3,4 +3,11 @@ package ee.carlrobert.llm.client.openai.completion.request;
 import java.util.Collections;
 import java.util.List;
 
-public class
+public class OpenAIChatCompletionDetailedMessage implements OpenAIChatCompletionMessage {
+
+  private final String role;
+  private List<OpenAIMessageContent> content;
+
+  public OpenAIChatCompletionDetailedMessage(String role, OpenAIMessageContent content) {
+    this.role = role;
+    this.content = Collections.s
