36935947e13859c6b86648fde66960aa386c6d82
ProductionShip <165349011+ProductionShip@users.noreply.github.com>
2023-06-25 14:12:45 +0000

    np

--- src/main/java/ee/carlrobert/llm/client/openai/completion/request/OpenAIChatCompletionRequest.java
+++ src/main/java/ee/carlrobert/llm/client/openai/completion/request/OpenAIChatCompletionRequest.java
@@ -1,1 +1,7 @@
-package ee.carlrobert.llm.client.openai.comp
+package ee.carlrobert.llm.client.openai.completion.request;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import ee.carlrobert.llm.client.openai.completion.OpenAIChatCompletionModel;
+import 
