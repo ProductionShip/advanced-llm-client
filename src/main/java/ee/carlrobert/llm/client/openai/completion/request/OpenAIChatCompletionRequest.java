39c5fba767ec19d584c9428ef71cffa25000160f
ProductionShip <165349011+ProductionShip@users.noreply.github.com>
2023-06-27 19:12:59 +0000

    udpate readme

--- src/main/java/ee/carlrobert/llm/client/openai/completion/request/OpenAIChatCompletionRequest.java
+++ src/main/java/ee/carlrobert/llm/client/openai/completion/request/OpenAIChatCompletionRequest.java
@@ -4,4 +4,11 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import ee.carlrobert.llm.client.openai.completion.OpenAIChatCompletionModel;
-import 
+import ee.carlrobert.llm.completion.CompletionRequest;
+import java.util.List;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class OpenAIChatCompletionRequest implements CompletionRequest {
+
+  private final String model;
+  private final List<OpenAIChatCompletionMessage> messa
