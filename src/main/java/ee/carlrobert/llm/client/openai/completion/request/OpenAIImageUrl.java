71391554596ee1aa3d84bf495c66a247ed93159e
ProductionShip <165349011+ProductionShip@users.noreply.github.com>
2023-07-08 09:43:39 +0000

    More stats

--- src/main/java/ee/carlrobert/llm/client/openai/completion/request/OpenAIImageUrl.java
+++ src/main/java/ee/carlrobert/llm/client/openai/completion/request/OpenAIImageUrl.java
@@ -4,4 +4,16 @@ import com.fasterxml.jackson.annotation.JsonEnumDefaultValue;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonValue;
-import java.util.Ba
+import java.util.Base64;
+
+/**
+ * Messages with image content are supported by OpenAIs Vision models.
+ */
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class OpenAIImageUrl {
+
+  private String url;
+
+  private ImageDetail detail;
+
+  public OpenAIImageUrl(String mediaType, byte[] imageData, ImageDetail det
