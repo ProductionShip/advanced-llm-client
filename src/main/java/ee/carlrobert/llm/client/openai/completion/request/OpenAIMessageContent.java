3a65cb2e42750d1672a67e1710300ac70642cba7
ProductionShip <165349011+ProductionShip@users.noreply.github.com>
2023-07-19 21:18:06 +0000

    regulator: wm831x: Set the new rather than old value for DVS VSEL

--- src/main/java/ee/carlrobert/llm/client/openai/completion/request/OpenAIMessageContent.java
+++ src/main/java/ee/carlrobert/llm/client/openai/completion/request/OpenAIMessageContent.java
@@ -1,4 +1,11 @@
 package ee.carlrobert.llm.client.openai.completion.request;
 
 import com.fasterxml.jackson.annotation.JsonSubTypes;
-import com.fasterxml.jackson.a
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+
+@JsonTypeInfo(
+    use = JsonTypeInfo.Id.NAME,
+    property = "type")
+@JsonSubTypes({
+    @JsonSubTypes.Type(value = OpenAIMessageTextContent.class, name = "text"),
+    @JsonSubTypes.Type(value = OpenAIMessageImageURLContent.class, name = "imag
