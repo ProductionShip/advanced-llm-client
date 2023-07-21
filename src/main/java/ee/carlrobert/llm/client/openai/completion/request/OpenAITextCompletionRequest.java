bd93ad2780cb02569fa7a4fbaec1684d9ce4087d
ProductionShip <165349011+ProductionShip@users.noreply.github.com>
2023-07-21 07:58:38 +0000

    updated npmignore

--- src/main/java/ee/carlrobert/llm/client/openai/completion/request/OpenAITextCompletionRequest.java
+++ src/main/java/ee/carlrobert/llm/client/openai/completion/request/OpenAITextCompletionRequest.java
@@ -39,3 +39,81 @@ public class OpenAITextCompletionRequest implements CompletionRequest {
 
   public String getPrompt() {
     return prompt;
+  }
+
+  public String getSuffix() {
+    return suffix;
+  }
+
+  public double getTemperature() {
+    return temperature;
+  }
+
+  public double getFrequencyPenalty() {
+    return frequencyPenalty;
+  }
+
+  public double getPresencePenalty() {
+    return presencePenalty;
+  }
+
+  public boolean isStream() {
+    return stream;
+  }
+
+  public static class Builder {
+
+    private final String prompt;
+
+    private String model = "gpt-3.5-turbo-instruct";
+    private String suffix;
+    private int maxTokens = 1000;
+    private double temperature = 0.9;
+    private double frequencyPenalty = 0;
+    private double presencePenalty = 0.6;
+    private boolean stream = true;
+
+    public Builder(String prompt) {
+      this.prompt = prompt;
+    }
+
+    public Builder setModel(String model) {
+      this.model = model;
+      return this;
+    }
+
+    public Builder setSuffix(String suffix) {
+      this.suffix = suffix;
+      return this;
+    }
+
+    public Builder setMaxTokens(int maxTokens) {
+      this.maxTokens = maxTokens;
+      return this;
+    }
+
+    public Builder setTemperature(double temperature) {
+      this.temperature = temperature;
+      return this;
+    }
+
+    public Builder setFrequencyPenalty(double frequencyPenalty) {
+      this.frequencyPenalty = frequencyPenalty;
+      return this;
+    }
+
+    public Builder setPresencePenalty(double presencePenalty) {
+      this.presencePenalty = presencePenalty;
+      return this;
+    }
+
+    public Builder setStream(boolean stream) {
+      this.stream = stream;
+      return this;
+    }
+
+    public OpenAITextCompletionRequest build() {
+      return new OpenAITextCompletionRequest(this);
+    }
+  }
+}
