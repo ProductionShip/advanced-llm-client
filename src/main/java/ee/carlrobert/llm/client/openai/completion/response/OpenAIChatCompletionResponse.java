6a04165be987fb20059de63a8376d73f581ea57c
ProductionShip <165349011+ProductionShip@users.noreply.github.com>
2023-07-25 15:59:47 +0000

    Makefile update

--- src/main/java/ee/carlrobert/llm/client/openai/completion/response/OpenAIChatCompletionResponse.java
+++ src/main/java/ee/carlrobert/llm/client/openai/completion/response/OpenAIChatCompletionResponse.java
@@ -1,3 +1,10 @@
 package ee.carlrobert.llm.client.openai.completion.response;
 
-import com.faste
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import ee.carlrobert.llm.completion.CompletionResponse;
+import java.util.List;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+publi
