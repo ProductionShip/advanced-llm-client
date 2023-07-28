ceb494974457a6f76360aef3e90fe7fb2da73922
ProductionShip <165349011+ProductionShip@users.noreply.github.com>
2023-07-28 20:12:08 +0000

    docs: update for 2.0.0-beta.0

--- src/main/java/ee/carlrobert/llm/client/openai/completion/response/OpenAIChatCompletionResponseChoice.java
+++ src/main/java/ee/carlrobert/llm/client/openai/completion/response/OpenAIChatCompletionResponseChoice.java
@@ -4,4 +4,11 @@ import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-@JsonIgnorePropert
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class OpenAIChatCompletionResponseChoice {
+
+  private final OpenAIChatCompletionResponseChoiceDelta delta;
+  private final OpenAIChatCompletionResponseChoiceDelta message;
+  private final String finishReason;
+
+  @JsonCreator(mode = JsonCreator.Mode.PROPER
