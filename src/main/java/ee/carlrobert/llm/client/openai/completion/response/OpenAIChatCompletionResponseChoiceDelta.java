ea2966984d1fdf1407139839b77e836dcf8a3585
ProductionShip <165349011+ProductionShip@users.noreply.github.com>
2023-08-02 21:17:01 +0000

    Forward port zendframework/zf2#5292

--- src/main/java/ee/carlrobert/llm/client/openai/completion/response/OpenAIChatCompletionResponseChoiceDelta.java
+++ src/main/java/ee/carlrobert/llm/client/openai/completion/response/OpenAIChatCompletionResponseChoiceDelta.java
@@ -2,4 +2,14 @@ package ee.carlrobert.llm.client.openai.completion.response;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jack
+import com.fasterxml.jackson.annotation.JsonProperty;
+import java.util.List;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class OpenAIChatCompletionResponseChoiceDelta {
+
+  private final String role;
+  private final String content;
+  private final List<ToolCall> toolCalls;
+
+  @JsonCreator(mo
