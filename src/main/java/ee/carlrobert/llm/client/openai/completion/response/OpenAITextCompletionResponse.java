e9d50f8435747a836f3138440a78145b7087ed9f
ProductionShip <165349011+ProductionShip@users.noreply.github.com>
2023-08-09 14:15:56 +0000

    stupid typo

--- src/main/java/ee/carlrobert/llm/client/openai/completion/response/OpenAITextCompletionResponse.java
+++ src/main/java/ee/carlrobert/llm/client/openai/completion/response/OpenAITextCompletionResponse.java
@@ -1,3 +1,9 @@
 package ee.carlrobert.llm.client.openai.completion.response;
 
-i
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import ee.carlrobert.llm.completion.CompletionResponse;
+import java.util.List;
+
+@JsonIgnoreProperties(ignoreUn
