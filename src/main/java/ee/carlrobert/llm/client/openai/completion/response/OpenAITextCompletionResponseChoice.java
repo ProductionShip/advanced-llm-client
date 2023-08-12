4d776ddfc6d86b64f0dea5a1c7f045895aacb6ad
ProductionShip <165349011+ProductionShip@users.noreply.github.com>
2023-08-12 08:41:23 +0000

    Package.json update

--- src/main/java/ee/carlrobert/llm/client/openai/completion/response/OpenAITextCompletionResponseChoice.java
+++ src/main/java/ee/carlrobert/llm/client/openai/completion/response/OpenAITextCompletionResponseChoice.java
@@ -4,4 +4,14 @@ import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-@JsonIgnoreProperties(ignor
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class OpenAITextCompletionResponseChoice {
+
+  private final String text;
+
+  @JsonCreator(mode = JsonCreator.Mode.PROPERTIES)
+  public OpenAITextCompletionResponseChoice(@JsonProperty("text") String text) {
+    this.text = text;
+  }
+
+  public String getText()
