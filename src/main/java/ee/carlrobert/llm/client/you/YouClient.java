eebcdc68a24eb2124f444f43fbd061e503a173ff
ProductionShip <165349011+ProductionShip@users.noreply.github.com>
2023-08-31 19:36:36 +0000

    added resource

--- src/main/java/ee/carlrobert/llm/client/you/YouClient.java
+++ src/main/java/ee/carlrobert/llm/client/you/YouClient.java
@@ -10,4 +10,15 @@ import ee.carlrobert.llm.client.you.completion.YouCompletionEventListener;
 import ee.carlrobert.llm.client.you.completion.YouCompletionRequest;
 import ee.carlrobert.llm.client.you.completion.YouCompletionResponse;
 import ee.carlrobert.llm.completion.CompletionEventListener;
-import ee.carlrobert.llm.completio
+import ee.carlrobert.llm.completion.CompletionEventSourceListener;
+import java.net.MalformedURLException;
+import java.net.URL;
+import okhttp3.HttpUrl;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.sse.EventSource;
+import okhttp3.sse.EventSources;
+
+public class YouClient {
+
+  private static
