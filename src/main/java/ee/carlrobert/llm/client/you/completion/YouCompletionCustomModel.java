3b7b7515a329b3675fd08935bbb670a8397ff913
ProductionShip <165349011+ProductionShip@users.noreply.github.com>
2023-10-05 18:02:24 +0000

    Change some text

--- src/main/java/ee/carlrobert/llm/client/you/completion/YouCompletionCustomModel.java
+++ src/main/java/ee/carlrobert/llm/client/you/completion/YouCompletionCustomModel.java
@@ -6,4 +6,12 @@ public enum YouCompletionCustomModel {
   GPT_4("gpt_4", "GPT-4"),
   GPT_4_TURBO("gpt_4_turbo", "GPT-4 Turbo"),
   CLAUDE_INSTANT("claude_instant", "Claude Instant"),
- 
+  CLAUDE_2("claude_2", "Claude 2"),
+  GEMINI_PRO("gemini_pro", "Gemini Pro"),
+  ZEPHYR("zephyr", "Zephyr (uncensored)");
+
+  private final String model;
+  private final String description;
+
+  YouCompletionCustomModel(String model, String description) {
+    this.model
