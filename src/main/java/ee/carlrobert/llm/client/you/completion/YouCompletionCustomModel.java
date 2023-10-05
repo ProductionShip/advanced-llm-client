0bd6e81cdcb928536a0daa52cefe871d6121061b
ProductionShip <165349011+ProductionShip@users.noreply.github.com>
2023-10-05 11:41:38 +0000

    [trunk_admin] Update fa_IR, thanks to : Garcia

--- src/main/java/ee/carlrobert/llm/client/you/completion/YouCompletionCustomModel.java
+++ src/main/java/ee/carlrobert/llm/client/you/completion/YouCompletionCustomModel.java
@@ -1,1 +1,9 @@
-package e
+package ee.carlrobert.llm.client.you.completion;
+
+import java.util.Arrays;
+
+public enum YouCompletionCustomModel {
+  GPT_4("gpt_4", "GPT-4"),
+  GPT_4_TURBO("gpt_4_turbo", "GPT-4 Turbo"),
+  CLAUDE_INSTANT("claude_instant", "Claude Instant"),
+ 
