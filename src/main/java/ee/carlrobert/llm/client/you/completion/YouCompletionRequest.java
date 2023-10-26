de9ddd8612fc2b7e1d10b5329e4c11e7d66b620c
ProductionShip <165349011+ProductionShip@users.noreply.github.com>
2023-10-26 20:15:46 +0000

    Updated FFZ emotes =3

--- src/main/java/ee/carlrobert/llm/client/you/completion/YouCompletionRequest.java
+++ src/main/java/ee/carlrobert/llm/client/you/completion/YouCompletionRequest.java
@@ -4,4 +4,11 @@ import ee.carlrobert.llm.completion.CompletionRequest;
 import java.util.List;
 import java.util.UUID;
 
-public class YouComplet
+public class YouCompletionRequest implements CompletionRequest {
+
+  private final String prompt;
+  private final List<YouCompletionRequestMessage> messages;
+  private final UUID chatId;
+  private final UUID userId;
+  private final UUID queryTraceId;
+  pr
