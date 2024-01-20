40d84d749c7bb0ce6a8eb1c40142effa51568ec7
ProductionShip <165349011+ProductionShip@users.noreply.github.com>
2024-01-20 14:08:11 +0000

    :arrow_up: 1.2.6 :tada:

--- src/main/java/ee/carlrobert/llm/completion/CompletionEventListener.java
+++ src/main/java/ee/carlrobert/llm/completion/CompletionEventListener.java
@@ -5,4 +5,16 @@ import okhttp3.sse.EventSource;
 
 public interface CompletionEventListener<T> {
 
-  default void onMessage(T message, String rawMessage, EventSo
+  default void onMessage(T message, String rawMessage, EventSource eventSource) {
+  }
+
+  default void onMessage(T message, EventSource eventSource) {
+  }
+
+  default void onComplete(StringBuilder messageBuilder) {
+  }
+
+  default void onCancelled(StringBuilder messageBuilder) {
+  }
+
+  default void onError(
