f50de925d0a7f20a8db12636d1a8aadffbeb0405
ProductionShip <165349011+ProductionShip@users.noreply.github.com>
2024-02-07 06:48:58 +0000

    Updating changes

--- src/main/java/ee/carlrobert/llm/completion/CompletionEventSourceListener.java
+++ src/main/java/ee/carlrobert/llm/completion/CompletionEventSourceListener.java
@@ -3,4 +3,12 @@ package ee.carlrobert.llm.completion;
 import static java.lang.String.format;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import ee.carlrobert.llm.client.openai.completion.ErrorDetails
+import ee.carlrobert.llm.client.openai.completion.ErrorDetails;
+import java.io.IOException;
+import java.net.SocketException;
+import java.net.SocketTimeoutException;
+import java.util.function.Consumer;
+import okhttp3.Response;
+import okhttp3.internal.http2.StreamResetException;
+import okhttp3.sse.EventSource;
+import okhttp3.sse.EventSou
