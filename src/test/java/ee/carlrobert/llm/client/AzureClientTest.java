e57ee573bd601dd9c9e64ebf43565af3cf2857b9
ProductionShip <165349011+ProductionShip@users.noreply.github.com>
2024-03-11 05:28:22 +0000

    fix theme

--- src/test/java/ee/carlrobert/llm/client/AzureClientTest.java
+++ src/test/java/ee/carlrobert/llm/client/AzureClientTest.java
@@ -168,3 +168,104 @@ class AzureClientTest extends BaseTest {
               0.1,
               0.1,
               List.of(Map.of("role", "user", "content", prompt)));
+      return List.of(
+          jsonMapResponse("choices", jsonArray(jsonMap("delta", jsonMap("role", "assistant")))),
+          jsonMapResponse("choices", jsonArray(jsonMap("delta", jsonMap("content", "Hello")))),
+          jsonMapResponse("choices", jsonArray(jsonMap("delta", jsonMap("content", "!")))));
+    });
+
+    new AzureClient.Builder("TEST_API_KEY",
+        new AzureCompletionRequestParams(
+            "TEST_RESOURCE",
+            "TEST_DEPLOYMENT_ID",
+            "TEST_API_VERSION"))
+        .setActiveDirectoryAuthentication(true)
+        .build()
+        .getChatCompletionAsync(
+            new OpenAIChatCompletionRequest.Builder(
+                List.of(new OpenAIChatCompletionStandardMessage("user", prompt)))
+                .setMaxTokens(500)
+                .setTemperature(0.5)
+                .setPresencePenalty(0.1)
+                .setFrequencyPenalty(0.1)
+                .setOverriddenPath("/v1/deployments/%s/completions?api_version=%s")
+                .build(),
+            new CompletionEventListener<String>() {
+              @Override
+              public void onMessage(String message, EventSource eventSource) {
+                resultMessageBuilder.append(message);
+              }
+
+              @Override
+              public void onComplete(StringBuilder messageBuilder) {
+                assertThat(messageBuilder.toString()).isEqualTo(resultMessageBuilder.toString());
+              }
+            });
+
+    await().atMost(5, SECONDS).until(() -> "Hello!".contentEquals(resultMessageBuilder));
+  }
+
+  @Test
+  void shouldListenForInvalidTokenErrorResponse() {
+    var errorMessageBuilder = new StringBuilder();
+    var errorResponse = jsonMapResponse(
+        e("statusCode", 401),
+        e("message", "Token is invalid"));
+    expectAzure((BasicHttpExchange) request -> {
+      assertThat(request.getUri().getPath()).isEqualTo(
+          "/openai/deployments/TEST_DEPLOYMENT_ID/chat/completions");
+      return new ResponseEntity(401, errorResponse);
+    });
+
+    new AzureClient.Builder("TEST_API_KEY",
+        new AzureCompletionRequestParams(
+            "TEST_RESOURCE",
+            "TEST_DEPLOYMENT_ID",
+            "TEST_API_VERSION"))
+        .build()
+        .getChatCompletionAsync(
+            new OpenAIChatCompletionRequest.Builder(
+                List.of(new OpenAIChatCompletionStandardMessage("user", "TEST_PROMPT")))
+                .build(),
+            new CompletionEventListener<String>() {
+              @Override
+              public void onError(ErrorDetails error, Throwable t) {
+                errorMessageBuilder.append(error.getMessage());
+              }
+            });
+
+    await().atMost(5, SECONDS).until(() -> "Token is invalid".contentEquals(errorMessageBuilder));
+  }
+
+  @Test
+  void shouldListenForInvalidResourceErrorResponse() {
+    var errorMessageBuilder = new StringBuilder();
+    var errorResponse = jsonMapResponse("error", jsonMap(
+        e("message", "Resource not found"),
+        e("code", "404")));
+    expectAzure((BasicHttpExchange) request -> {
+      assertThat(request.getUri().getPath()).isEqualTo(
+          "/openai/deployments/TEST_DEPLOYMENT_ID/chat/completions");
+      return new ResponseEntity(404, errorResponse);
+    });
+
+    new AzureClient.Builder("TEST_API_KEY",
+        new AzureCompletionRequestParams(
+            "TEST_RESOURCE",
+            "TEST_DEPLOYMENT_ID",
+            "TEST_API_VERSION"))
+        .build()
+        .getChatCompletionAsync(
+            new OpenAIChatCompletionRequest.Builder(
+                List.of(new OpenAIChatCompletionStandardMessage("user", "TEST_PROMPT")))
+                .build(),
+            new CompletionEventListener<String>() {
+              @Override
+              public void onError(ErrorDetails error, Throwable t) {
+                errorMessageBuilder.append(error.getMessage());
+              }
+            });
+
+    await().atMost(5, SECONDS).until(() -> "Resource not found".contentEquals(errorMessageBuilder));
+  }
+}
---
+++ src/test/java/ee/carlrobert/llm/client/BaseTest.java
@@ -0,0 +1,6 @@
+package ee.carlrobert.llm.client;
+
+import ee.carlrobert.llm.client.mixin.ExternalServiceTestMixin;
+import org.junit.jupiter.api.AfterEach;
+
+public abstract c
