ea47fb4108584aa1c98d108174043926a0fd88fa
ProductionShip <165349011+ProductionShip@users.noreply.github.com>
2024-03-15 16:10:44 +0000

    Remove deprecations

--- src/test/java/ee/carlrobert/llm/client/CodeGPTClientTest.java
+++ src/test/java/ee/carlrobert/llm/client/CodeGPTClientTest.java
@@ -8,4 +8,9 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.tuple;
 import static org.awaitility.Awaitility.await;
 
-import com.fasterxml.jackson.databi
+import com.fasterxml.jackson.databind.ObjectMapper;
+import ee.carlrobert.llm.client.codegpt.CodeGPTClient;
+import ee.carlrobert.llm.client.http.ResponseEntity;
+import ee.carlrobert.llm.client.http.exchange.BasicHttpExchange;
+import ee.carlrobert.llm.client.http.exchange.StreamHttpExchange;
+import ee.carlrobert.llm.client.openai.O
