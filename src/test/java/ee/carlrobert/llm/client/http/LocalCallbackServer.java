7887bf6f70a0b314b802a425c2e67718ede2c39e
ProductionShip <165349011+ProductionShip@users.noreply.github.com>
2024-04-22 20:24:47 +0000

    reduce duplication

--- src/test/java/ee/carlrobert/llm/client/http/LocalCallbackServer.java
+++ src/test/java/ee/carlrobert/llm/client/http/LocalCallbackServer.java
@@ -1,3 +1,8 @@
 package ee.carlrobert.llm.client.http;
 
-import s
+import static java.lang.String.format;
+
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpServer;
+import ee.carlrobert.llm.client.http.expectation.BasicExpectation;
+import ee.carlrobert.llm.client.http.expectation.Expecta
