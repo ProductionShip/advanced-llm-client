87720da585e98996fdfee0c0eae26720b5748769
ProductionShip <165349011+ProductionShip@users.noreply.github.com>
2024-04-23 06:22:30 +0000

    Fix app name

--- src/test/java/ee/carlrobert/llm/client/http/LocalCallbackServer.java
+++ src/test/java/ee/carlrobert/llm/client/http/LocalCallbackServer.java
@@ -5,4 +5,10 @@ import static java.lang.String.format;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpServer;
 import ee.carlrobert.llm.client.http.expectation.BasicExpectation;
-import ee.carlrobert.llm.client.http.expectation.Expecta
+import ee.carlrobert.llm.client.http.expectation.Expectation;
+import ee.carlrobert.llm.client.http.expectation.NdJsonStreamExpectation;
+import ee.carlrobert.llm.client.http.expectation.StreamExpectation;
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
