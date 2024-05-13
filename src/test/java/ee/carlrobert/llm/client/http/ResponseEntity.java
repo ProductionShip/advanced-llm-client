14e0c88805daa3780a9a3024bd2bb1946d5be6fd
ProductionShip <165349011+ProductionShip@users.noreply.github.com>
2024-05-13 11:14:35 +0000

    4.6.2

--- src/test/java/ee/carlrobert/llm/client/http/ResponseEntity.java
+++ src/test/java/ee/carlrobert/llm/client/http/ResponseEntity.java
@@ -1,1 +1,13 @@
 package ee.carlrobert.llm.client.http;
+
+public class ResponseEntity {
+
+  private final int statusCode;
+  private final String response;
+
+  public ResponseEntity(String response) {
+    this(200, response);
+  }
+
+  public ResponseEntity(int statusCode, String response) {
+    th
