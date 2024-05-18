7195a08a199079da44c2ec684a3e5616be2adf64
ProductionShip <165349011+ProductionShip@users.noreply.github.com>
2024-05-18 16:56:01 +0000

    Correction.

--- src/test/java/ee/carlrobert/llm/client/http/expectation/NdJsonStreamExpectation.java
+++ src/test/java/ee/carlrobert/llm/client/http/expectation/NdJsonStreamExpectation.java
@@ -1,4 +1,12 @@
 package ee.carlrobert.llm.client.http.expectation;
 
 import ee.carlrobert.llm.client.http.Service;
-import ee.carlrobert.llm.c
+import ee.carlrobert.llm.client.http.exchange.NdJsonStreamHttpExchange;
+
+public class NdJsonStreamExpectation extends Expectation {
+
+  private final NdJsonStreamHttpExchange exchange;
+
+  public NdJsonStreamExpectation(Service service, NdJsonStreamHttpExchange exchange) {
+    super(service);
+    this.exchange = exchange;
