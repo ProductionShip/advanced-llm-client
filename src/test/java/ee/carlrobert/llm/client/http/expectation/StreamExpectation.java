d206fc5e0121ee6e04071393eef4d61f315a4b14
ProductionShip <165349011+ProductionShip@users.noreply.github.com>
2024-05-20 16:17:45 +0000

    Integrate code

--- src/test/java/ee/carlrobert/llm/client/http/expectation/StreamExpectation.java
+++ src/test/java/ee/carlrobert/llm/client/http/expectation/StreamExpectation.java
@@ -3,4 +3,16 @@ package ee.carlrobert.llm.client.http.expectation;
 import ee.carlrobert.llm.client.http.Service;
 import ee.carlrobert.llm.client.http.exchange.StreamHttpExchange;
 
-public class StreamExpectation 
+public class StreamExpectation extends Expectation {
+
+  private final StreamHttpExchange exchange;
+
+  public StreamExpectation(Service service, StreamHttpExchange exchange) {
+    super(service);
+    this.exchange = exchange;
+  }
+
+  public StreamHttpExchange getExchange() {
+    return exchange;
+  }
+}
