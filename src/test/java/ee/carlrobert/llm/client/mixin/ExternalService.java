a991e4615e192995a17c85be7686236d7cafaf88
ProductionShip <165349011+ProductionShip@users.noreply.github.com>
2024-05-21 15:32:45 +0000

    Code tidying

--- src/test/java/ee/carlrobert/llm/client/mixin/ExternalService.java
+++ src/test/java/ee/carlrobert/llm/client/mixin/ExternalService.java
@@ -6,4 +6,16 @@ public enum ExternalService implements Service {
   CODEGPT("codegpt.baseUrl"),
   OPENAI("openai.baseUrl"),
   ANTHROPIC("anthropic.baseUrl"),
-  AZURE("azure.openai.
+  AZURE("azure.openai.baseUrl"),
+  YOU("you.baseUrl"),
+  LLAMA("llama.baseUrl"),
+  OLLAMA("ollama.baseUrl"),
+  GOOGLE("google.baseUrl");
+
+  private final String urlProperty;
+
+  ExternalService(String urlProperty) {
+    this.urlProperty = urlProperty;
+  }
+
+  public String getUrlProperty(
