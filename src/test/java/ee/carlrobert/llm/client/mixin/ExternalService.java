6aae7698c7a54d0701414f844218ed1d9a011c65
ProductionShip <165349011+ProductionShip@users.noreply.github.com>
2024-05-21 15:18:02 +0000

    Added test files.

--- src/test/java/ee/carlrobert/llm/client/mixin/ExternalService.java
+++ src/test/java/ee/carlrobert/llm/client/mixin/ExternalService.java
@@ -1,1 +1,9 @@
-package ee.c
+package ee.carlrobert.llm.client.mixin;
+
+import ee.carlrobert.llm.client.http.Service;
+
+public enum ExternalService implements Service {
+  CODEGPT("codegpt.baseUrl"),
+  OPENAI("openai.baseUrl"),
+  ANTHROPIC("anthropic.baseUrl"),
+  AZURE("azure.openai.
